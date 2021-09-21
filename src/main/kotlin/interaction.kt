493a79265f4c4b9052c7f8512af10aec607bc8b3
EgorShibaev <mister.egor03@gmail.com>
2021-09-21 19:13:51 +0300

    Functions for encrypt and decipher are written. Tests for them are added.

--- src/main/kotlin/interaction.kt
+++ src/main/kotlin/interaction.kt
@@ -1,5 +1,62 @@
 import java.io.File
 
+fun encrypt(text: String, key: String): String {
+	val rows = key.length
+	val columns = (text.length + rows - 1) / rows + 1
+	val table = key.map { (listOf(it) + List(columns - 1) { 0.toChar() }).toMutableList() }
+	var currRow = 0
+	var currCol = 1
+	text.forEach {
+		table[currRow][currCol] = it
+		currCol++
+		if (currCol == columns) {
+			currCol = 1
+			currRow++
+		}
+	}
+	val sortedTable = table.sortedBy { it.first() }
+	val result = StringBuilder()
+	for (column in 1 until columns)
+		for (row in 0 until rows)
+			result.append(sortedTable[row][column])
+	return result.toString()
+}
+
+fun reverseSort(key : String): List<Int> {
+	val sort = key.withIndex().sortedBy { it.value }
+	val reverseSort = MutableList(key.length) { 0 }
+	sort.withIndex().forEach {
+		reverseSort[it.value.index] = it.index
+	}
+	return reverseSort
+}
+
+fun decipher(text: String, key: String): String {
+	val rows = key.length
+	val columns = text.length / rows + 1
+	val table = key.map { (listOf(it) + List(columns - 1) { 0.toChar() }).toMutableList() }
+	var currRow = 0
+	var currCol = 1
+	text.forEach {
+		table[currRow][currCol] = it
+		currRow++
+		if (currRow == rows) {
+			currRow = 0
+			currCol++
+		}
+	}
+	// reverse sort
+	val reverseSortOrder = reverseSort(key)
+	val sortedTable = (0 until rows).map { table[reverseSortOrder[it]] }
+	val result = StringBuilder()
+	for (row in 0 until rows)
+		for (column in 1 until columns)
+			result.append(sortedTable[row][column])
+	while (result.last() == 0.toChar())
+		result.delete(result.length - 1, result.length)
+	return result.toString()
+}
+
 fun readBase2(): Map<String, String> {
 	val lines = File("database").readLines()
 	val result = mutableMapOf<String, String>()
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,9 +1,26 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
-class Test {
+class TestEncryptDecipher {
 
 	@Test
 	fun test1() {
-		assert(true)
+		val text = "some text is here"
+		val key = "key"
+		assertEquals(text, decipher(encrypt(text, key), key))
+	}
+
+	@Test
+	fun test2() {
+		val text = "text\n with\n few lines\n "
+		val key = "KEY"
+		assertEquals(text, decipher(encrypt(text, key), key))
+	}
+
+	@Test
+	fun test3() {
+		val text = "one  two three for     five \n six seven     eight\n"
+		val key = "some long key abcdef"
+		assertEquals(text, decipher(encrypt(text, key), key))
 	}
 }
