0499069a3ee54f47bf9a4e4e1e8fec30e5dc1fc5
EgorShibaev <mister.egor03@gmail.com>
2021-10-03 10:07:43 +0300

    Refactoring (Using OOP for rollback command)

--- src/main/kotlin/OperationsBackUp.kt
+++ src/main/kotlin/OperationsBackUp.kt
@@ -1,38 +1,21 @@
 /**
  * In this file are classes which organize storage of operation and functions to roll back
- * this operation. There are classes for each operation with method for rollback.
- * There is class Operation which contains nullable field of all extra classes.
+ * this operation. There is base class Operation with abstract method rollback. For each
+ * change command there is a class which inherits Operation with override method rollback.
  * */
-data class Operation(
-	val insertOperation: InsertOperation? = null,
-	val eraseOperation: EraseOperation? = null,
-	val clearOperation: ClearOperation? = null,
-	val eraseRegexOperation: EraseRegexOperation? = null,
-	val createGroupOperation: CreateGroupOperation? = null,
-	val eraseGroupOperation: EraseGroupOperation? = null,
-	val insertInGroupOperation: InsertInGroupOperation? = null,
-	val eraseFromGroupOperation: EraseFromGroupOperation? = null
-) {
-	fun rollback(database: Database) {
-		insertOperation?.rollback(database)
-		eraseOperation?.rollback(database)
-		clearOperation?.rollback(database)
-		eraseRegexOperation?.rollback(database)
-		createGroupOperation?.rollback(database)
-		eraseGroupOperation?.rollback(database)
-		insertInGroupOperation?.rollback(database)
-		eraseFromGroupOperation?.rollback(database)
-	}
+sealed class Operation {
+	abstract fun rollback(database: Database)
 }
 
-data class InsertOperation(val key: String) {
-	fun rollback(database: Database) {
+data class InsertOperation(val key: String) : Operation() {
+	override fun rollback(database: Database) {
 		database.content.remove(key)
+
 	}
 }
 
-data class EraseOperation(val key: String, val value: String, val namesOfGroups: List<String>) {
-	fun rollback(database: Database) {
+data class EraseOperation(val key: String, val value: String, val namesOfGroups: List<String>) : Operation() {
+	override fun rollback(database: Database) {
 		database.content[key] = value
 		namesOfGroups.forEach {
 			database.groups.getValue(it).add(key)
@@ -40,43 +23,54 @@ data class EraseOperation(val key: String, val value: String, val namesOfGroups:
 	}
 }
 
-data class ClearOperation(val databaseBackUp: Database) {
-	fun rollback(database: Database) {
+data class ClearOperation(val databaseBackUp: Database) : Operation() {
+	override fun rollback(database: Database) {
 		database.content.putAll(databaseBackUp.content)
 		database.groups.putAll(databaseBackUp.groups)
 	}
 }
 
-data class EraseRegexOperation(val listOfEraseOperation: List<EraseOperation>) {
-	fun rollback(database: Database) {
+data class EraseRegexOperation(val listOfEraseOperation: List<EraseOperation>) : Operation() {
+	override fun rollback(database: Database) {
 		listOfEraseOperation.forEach {
 			it.rollback(database)
 		}
 	}
 }
 
-data class CreateGroupOperation(val name: String) {
-	fun rollback(database: Database) {
+data class CreateGroupOperation(val name: String) : Operation() {
+	override fun rollback(database: Database) {
 		database.groups.remove(name)
 	}
 }
 
-data class EraseGroupOperation(val name: String, val content: List<String>) {
-	fun rollback(database: Database) {
+data class EraseGroupOperation(val name: String, val content: List<String>) : Operation() {
+	override fun rollback(database: Database) {
 		database.groups[name] = content.toMutableList()
 	}
 }
 
-data class InsertInGroupOperation(val key: String, val groupName: String) {
-	fun rollback(database: Database) {
+data class InsertInGroupOperation(val key: String, val groupName: String) : Operation() {
+	override fun rollback(database: Database) {
 		database.groups.getValue(groupName).remove(key)
 	}
 }
 
-data class EraseFromGroupOperation(val key: String, val groupName: String) {
-	fun rollback(database: Database) {
+data class EraseFromGroupOperation(val key: String, val groupName: String) : Operation() {
+	override fun rollback(database: Database) {
 		database.groups.getValue(groupName).add(key)
 	}
+}
+
+data class UpdateOperation(val insertOperation: InsertOperation, val eraseOperation: EraseOperation) : Operation() {
+	override fun rollback(database: Database) {
+		insertOperation.rollback(database)
+		eraseOperation.rollback(database)
+	}
+}
+
+object NotAChangeOperation : Operation() {
+	override fun rollback(database: Database) = Unit
 }
 
 /**
@@ -87,32 +81,27 @@ fun createOperationForCommand(database: Database, command: Pair<Command, List<St
 	val content = database.content
 	val groups = database.groups
 	return when (command.first) {
-		Command.INSERT -> Operation(InsertOperation(args[0]))
-		Command.CLEAR -> Operation(clearOperation = ClearOperation(Database(HashMap(content), HashMap(groups))))
-		Command.CREATE_GROUP -> Operation(createGroupOperation = CreateGroupOperation(args[0]))
-		Command.ERASE_GROUP -> Operation(eraseGroupOperation = EraseGroupOperation(args[0], groups.getValue(args[0])))
-		Command.INSERT_IN_GROUP -> Operation(insertInGroupOperation = InsertInGroupOperation(args[0], args[1]))
-		Command.ERASE_FROM_GROUP -> Operation(eraseFromGroupOperation = EraseFromGroupOperation(args[1], args[0]))
-		Command.ERASE -> Operation(
-			eraseOperation = EraseOperation(
-				args[0],
-				content.getValue(args[0]),
-				groups.filter { args[0] in it.value }.keys.toList()
-			)
+		Command.INSERT -> InsertOperation(args[0])
+		Command.CLEAR -> ClearOperation(Database(HashMap(content), HashMap(groups)))
+		Command.CREATE_GROUP -> CreateGroupOperation(args[0])
+		Command.ERASE_GROUP -> EraseGroupOperation(args[0], groups.getValue(args[0]))
+		Command.INSERT_IN_GROUP -> InsertInGroupOperation(args[0], args[1])
+		Command.ERASE_FROM_GROUP -> EraseFromGroupOperation(args[1], args[0])
+		Command.ERASE -> EraseOperation(
+			args[0],
+			content.getValue(args[0]),
+			groups.filter { args[0] in it.value }.keys.toList()
 		)
 		Command.ERASE_REGEX ->
-			Operation(
-				eraseRegexOperation =
-				EraseRegexOperation(database.content.keys.filter { it.matches(command.second[0].toRegex()) }.map {
-					val value = content.getValue(it)
-					val nameGroups = groups.filter { group -> args[0] in group.value }.keys.toList()
-					EraseOperation(it, value, nameGroups)
-				})
-			)
-		Command.UPDATE -> Operation(
+			EraseRegexOperation(database.content.keys.filter { it.matches(command.second[0].toRegex()) }.map {
+				val value = content.getValue(it)
+				val nameGroups = groups.filter { group -> args[0] in group.value }.keys.toList()
+				EraseOperation(it, value, nameGroups)
+			})
+		Command.UPDATE -> UpdateOperation(
 			InsertOperation(args[0]),
 			EraseOperation(args[0], content.getValue(args[0]), groups.filter { args[0] in it.value }.keys.toList())
 		)
-		else -> Operation()
+		else -> NotAChangeOperation
 	}
 }
