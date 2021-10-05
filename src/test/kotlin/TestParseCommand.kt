c7c6d084242070928bd383ae6794463097499c8c
EgorShibaev <mister.egor03@gmail.com>
2021-10-05 16:15:07 +0300

    Add tests to exstra cases

--- src/test/kotlin/TestParseCommand.kt
+++ src/test/kotlin/TestParseCommand.kt
@@ -23,24 +23,40 @@ class TestParseCommand {
 	fun testNullInput() {
 		val input = null
 		val command = parseCommand(input)
-		assertEquals(command, null)
-		assertEquals(getOutput(), "")
+		assertEquals(null, command)
+		assertEquals("", getOutput())
 	}
 
 	@Test
 	fun testEmptyInput() {
 		val input = ""
 		val command = parseCommand(input)
-		assertEquals(command, null)
-		assertEquals(getOutput(), "")
+		assertEquals(null, command)
+		assertEquals("", getOutput())
+	}
+
+	@Test
+	fun testEmptyValue() {
+		val input = "insert a -> "
+		val command = parseCommand(input)
+		assertEquals(Pair(Command.INSERT, listOf("a", "")), command)
+		assertEquals("", getOutput())
+	}
+
+	@Test
+	fun testArrowInInput() {
+		val input = "insert a -> b -> c -> d"
+		val command = parseCommand(input)
+		assertEquals(Pair(Command.INSERT, listOf("a", "b -> c -> d")), command)
+		assertEquals("", getOutput())
 	}
 
 	@Test
 	fun testIncorrectInput() {
 		val input = "bom bom bom"
 		val command = parseCommand(input)
-		assertEquals(command, null)
-		assertEquals(getOutput(), "Incorrect command\n")
+		assertEquals(null, command)
+		assertEquals("Incorrect command\n", getOutput())
 	}
 
 	@Test
@@ -48,87 +64,87 @@ class TestParseCommand {
 		val input = "contentOfAllGroUps"
 		val command = parseCommand(input)
 		assertEquals(command, null)
-		assertEquals(getOutput(), "Incorrect command\nMaybe you mean contentOfAllGroups\n")
+		assertEquals("Incorrect command\nMaybe you mean contentOfAllGroups\n", getOutput())
 	}
 
 	@Test
 	fun testTwoTypoInput() {
 		val input = "insel a -> b"
 		val command = parseCommand(input)
-		assertEquals(command, null)
-		assertEquals(getOutput(), "Incorrect command\nMaybe you mean insert\n")
+		assertEquals(null, command)
+		assertEquals("Incorrect command\nMaybe you mean insert\n", getOutput())
 	}
 
 	@Test
 	fun testTreeTypoInput() {
 		val input = "er??? a"
 		val command = parseCommand(input)
-		assertEquals(command, null)
-		assertEquals(getOutput(), "Incorrect command\n")
+		assertEquals(null, command)
+		assertEquals("Incorrect command\n", getOutput())
 	}
 
 	@Test
 	fun testCorrectInsert() {
 		val input = "insert a -> b"
 		val command = parseCommand(input)
-		assertEquals(command, Pair(Command.INSERT, listOf("a", "b")))
-		assertEquals(getOutput(), "")
+		assertEquals(Pair(Command.INSERT, listOf("a", "b")), command)
+		assertEquals("", getOutput())
 	}
 
 	@Test
 	fun testCorrectErase() {
 		val input = "erase key with spaces"
 		val command = parseCommand(input)
-		assertEquals(command, Pair(Command.ERASE, listOf("key with spaces")))
-		assertEquals(getOutput(), "")
+		assertEquals(Pair(Command.ERASE, listOf("key with spaces")), command)
+		assertEquals("", getOutput())
 	}
 
 	@Test
 	fun testCorrectUpdate() {
 		val input = "   	update key 1 -> value 1  "
 		val command = parseCommand(input)
-		assertEquals(command, Pair(Command.UPDATE, listOf("key 1", "value 1")))
-		assertEquals(getOutput(), "")
+		assertEquals(Pair(Command.UPDATE, listOf("key 1", "value 1")), command)
+		assertEquals("", getOutput())
 	}
 
 	@Test
 	fun testCorrectFindRegex() {
 		val input = "findRegex .{4,}"
 		val command = parseCommand(input)
-		assertEquals(command, Pair(Command.FIND_REGEX, listOf(".{4,}")))
-		assertEquals(getOutput(), "")
+		assertEquals(Pair(Command.FIND_REGEX, listOf(".{4,}")), command)
+		assertEquals("", getOutput())
 	}
 
 	@Test
 	fun testCorrectContent() {
 		val input = "content"
 		val command = parseCommand(input)
-		assertEquals(command, Pair(Command.CONTENT, listOf()))
-		assertEquals(getOutput(), "")
+		assertEquals(Pair(Command.CONTENT, listOf()), command)
+		assertEquals("", getOutput())
 	}
 
 	@Test
 	fun testWrongCountOfArgumentsForInsert() {
 		val input = "insert a"
 		val command = parseCommand(input)
-		assertEquals(command, null)
-		assertEquals(getOutput(), "Wrong count of arguments\n")
+		assertEquals(null, command)
+		assertEquals("Wrong count of arguments\n", getOutput())
 	}
 
 	@Test
 	fun testWrongCountOfArgumentsForContentOfAllGroups() {
 		val input = "contentOfAllGroups group"
 		val command = parseCommand(input)
-		assertEquals(command, null)
-		assertEquals(getOutput(), "Wrong count of arguments\n")
+		assertEquals(null, command)
+		assertEquals("Wrong count of arguments\n", getOutput())
 	}
 
 	@Test
 	fun testWrongCountOfArgumentsForEraseRegex() {
 		val input = "eraseRegex  "
 		val command = parseCommand(input)
-		assertEquals(command, null)
-		assertEquals(getOutput(), "Wrong count of arguments\n")
+		assertEquals(null, command)
+		assertEquals("Wrong count of arguments\n", getOutput())
 	}
 
 	private fun getOutput() = stream.toString().replace("\r", "")
